334233ea3661210b2af8cb9ea6d5dc9e24d9e15a
jeppeter <jeppeter@gmail.com>
2023-12-14 11:48:30 +0800

    to compile ok for bind_server

--- src/sockhdl_windows.rs
+++ src/sockhdl_windows.rs
@@ -1,7 +1,7 @@
 
 use winapi::um::winnt::{HANDLE,PVOID,LPCWSTR};
-use winapi::um::winsock2::{SOL_SOCKET,SO_REUSEADDR,setsockopt,socket,SOCKET,INVALID_SOCKET,closesocket,SOCKET_ERROR,htons,bind,LPWSAOVERLAPPED,WSAOVERLAPPED,LPWSAOVERLAPPED_COMPLETION_ROUTINE,WSAIoctl,WSADATA,WSADESCRIPTION_LEN,WSASYS_STATUS_LEN,WSAStartup,WSACleanup,u_long,ioctlsocket,FIONBIO};
-use winapi::um::mswsock::{LPFN_CONNECTEX,WSAID_CONNECTEX};
+use winapi::um::winsock2::{SOL_SOCKET,SO_REUSEADDR,setsockopt,socket,SOCKET,INVALID_SOCKET,closesocket,SOCKET_ERROR,htons,bind,LPWSAOVERLAPPED,WSAOVERLAPPED,LPWSAOVERLAPPED_COMPLETION_ROUTINE,WSAIoctl,WSADATA,WSADESCRIPTION_LEN,WSASYS_STATUS_LEN,WSAStartup,WSACleanup,u_long,ioctlsocket,FIONBIO,WSAGetLastError,listen};
+use winapi::um::mswsock::{LPFN_ACCEPTEX,WSAID_ACCEPTEX};
 use winapi::shared::ws2def::*;
 use winapi::shared::inaddr::*;
 use winapi::shared::guiddef::{GUID};
@@ -9,10 +9,10 @@ use winapi::um::ws2tcpip::*;
 use winapi::um::synchapi::*;
 use winapi::um::ioapiset::{CancelIoEx};
 use winapi::um::errhandlingapi::{GetLastError,SetLastError};
-use winapi::um::minwinbase::{OVERLAPPED,LPSECURITY_ATTRIBUTES,SECURITY_ATTRIBUTES,LPOVERLAPPED};
+use winapi::um::minwinbase::{OVERLAPPED,LPSECURITY_ATTRIBUTES,SECURITY_ATTRIBUTES};
 use winapi::um::handleapi::{CloseHandle};
 use winapi::shared::minwindef::{MAKEWORD,WORD,LOBYTE,HIBYTE,BOOL,DWORD,TRUE,FALSE,LPVOID,LPDWORD};
-use winapi::ctypes::{c_int,c_void,c_char};
+use winapi::ctypes::{c_int,c_char};
 
 use super::{evtcall_error_class,evtcall_new_error};
 use super::consts_windows::{NULL_HANDLE_VALUE};
@@ -41,7 +41,7 @@ pub struct SockHandle {
 	wrov :OVERLAPPED,
 	inacc :i32,
 	accov :OVERLAPPED,
-	acceptfunc : LPFN_CONNECTEX,
+	acceptfunc : LPFN_ACCEPTEX,
 	localaddr : String,
 	localport : u32,
 }
@@ -57,6 +57,21 @@ macro_rules! get_errno {
 		let mut retv :i32 ;
 		unsafe {
 			retv = GetLastError() as i32;
+		}
+		if retv != 0 {
+			retv = -retv;
+		} else {
+			retv = -1;
+		}
+		retv
+	}};
+}
+
+macro_rules! get_wsa_errno {
+	() => {{
+		let mut retv :i32 ;
+		unsafe {
+			retv = WSAGetLastError() as i32;
 		}
 		if retv != 0 {
 			retv = -retv;
@@ -196,10 +211,65 @@ impl SockHandle {
 		}
 	}
 
+	fn _bind_addr(&mut self,ipaddr :&str, port :u32) -> Result<(),Box<dyn Error>> {
+		let mut name :SOCKADDR_IN = unsafe {std::mem::zeroed()};
+		let ret :i32;
+		let iret :c_int;
+		let namelen :c_int;
+		name.sin_family = AF_INET as u16;
+		if ipaddr.len() > 0 {
+			unsafe {
+				let _pv : PVOID = ((&mut name.sin_addr) as * mut IN_ADDR) as PVOID;
+				let _addr = ipaddr.as_bytes().as_ptr() as * const i8;
+				inet_pton(AF_INET,_addr,_pv);
+			}			
+		} else {
+			name.sin_addr = unsafe {std::mem::zeroed()};
+		}
+		if port != 0 {
+			name.sin_port = unsafe {htons(port as u16)};	
+		} else {
+			name.sin_port = 0;
+		}
+		
+		namelen = std::mem::size_of::<SOCKADDR_IN>() as c_int;
+		unsafe {
+			let _pv :*const SOCKADDR = (&name as *const SOCKADDR_IN) as *const SOCKADDR;
+			iret = bind(self.sock,_pv, namelen);
+		}
+
+		if iret == SOCKET_ERROR {
+			ret = get_wsa_errno!();
+			evtcall_new_error!{SockHandleError,"bind [{}:{}] error {}",ipaddr,port,ret}
+		}
+		Ok(())
+	}
+
+	fn _get_accept_func(&mut self, guid :&GUID) -> Result<(),Box<dyn Error>> {
+		let mut iret :c_int;
+		let mut dret :DWORD = 0;
+		unsafe {
+			let _funcptr :LPVOID = (&mut self.acceptfunc as *mut LPFN_ACCEPTEX ) as LPVOID;
+			let _funcsize:DWORD = std::mem::size_of::<LPFN_ACCEPTEX>() as DWORD;
+			let _guidptr :LPVOID = (guid as *const GUID) as LPVOID;
+			let _guidsize :DWORD = std::mem::size_of::<GUID>() as DWORD;
+			let _retptr :LPDWORD = &mut dret;
+			let _ptrov :LPWSAOVERLAPPED = std::ptr::null::<WSAOVERLAPPED>() as LPWSAOVERLAPPED;
+			let _fncall :LPWSAOVERLAPPED_COMPLETION_ROUTINE = None;
+			iret = WSAIoctl(self.sock,SIO_GET_EXTENSION_FUNCTION_POINTER,_guidptr,_guidsize,_funcptr,_funcsize,_retptr,_ptrov,_fncall);
+		}
+
+		if iret != 0 {
+			iret = get_wsa_errno!();
+			evtcall_new_error!{SockHandleError,"cannot get WSAIoctl error {}",iret}
+		}
+		Ok(())
+	}
+
 	#[allow(dead_code)]
 	#[allow(unused_variables)]
 	#[allow(unused_mut)]
-	pub fn bind_server(ipaddr :&str,port :u32,localip :&str,localport :u32,connected :bool) -> Result<Self,Box<dyn Error>> {
+	pub fn bind_server(ipaddr :&str,port :u32,backlog : i32) -> Result<Self,Box<dyn Error>> {
 		if ipaddr.len() == 0 || port == 0 {
 			evtcall_new_error!{SockHandleError,"not valid ipaddr [{}] or port [{}]",ipaddr,port}
 		}
@@ -208,9 +278,7 @@ impl SockHandle {
 		let mut iret :c_int;
 		let mut opt :c_int;
 		let mut block :u_long;
-		let mut name :SOCKADDR_IN = unsafe {std::mem::zeroed()};
-		let namelen :c_int;
-		let accguid:GUID = WSAID_CONNECTEX;
+		let accguid:GUID = WSAID_ACCEPTEX;
 		let mut dret :DWORD = 0;
 		let mut bret :BOOL;
 
@@ -222,7 +290,6 @@ impl SockHandle {
 
 		if retv.sock == INVALID_SOCKET {
 			ret = get_errno!();
-			retv.free();
 			evtcall_new_error!{SockHandleError,"cannot socket error {}",ret}
 		}
 
@@ -235,7 +302,6 @@ impl SockHandle {
 
 		if iret == SOCKET_ERROR {
 			ret = get_errno!();
-			retv.free();
 			evtcall_new_error!{SockHandleError,"cannot setopt reuse error {}",ret}
 		}
 
@@ -248,90 +314,23 @@ impl SockHandle {
 
 		if iret == SOCKET_ERROR {
 			ret = get_errno!();
-			retv.free();
 			evtcall_new_error!{SockHandleError,"cannot set non-block error {}",ret}
 		}
 
-
-		name.sin_family = AF_INET as u16;
-		if localip.len() != 0 {
-			let pv : PVOID = ((&mut name.sin_addr) as * mut IN_ADDR) as PVOID;
-			unsafe {
-				inet_pton(AF_INET,localip.as_bytes().as_ptr() as * const i8,pv);
-			}
-		} else {
-			name.sin_addr = unsafe {std::mem::zeroed()};
-		}
-
-		if localport != 0 {
-			name.sin_port = unsafe {htons(localport as u16)};	
-		} else {
-			name.sin_port = 0;
-		}
-
-		namelen = std::mem::size_of::<SOCKADDR_IN>() as c_int;
-		unsafe {
-			let pv :*const SOCKADDR = (&name as *const SOCKADDR_IN) as *const SOCKADDR;
-			iret = bind(retv.sock,pv, namelen);
-		}
-
-		if iret == SOCKET_ERROR {
-			ret = get_errno!();
-			retv.free();
-			evtcall_new_error!{SockHandleError,"bind [{}:{}] error {}",localip,localport,ret}
-		}
+		retv._bind_addr(ipaddr,port)?;
 
 		create_event_safe!(retv.connov.hEvent,retv,"connov handle");
 
-		name = unsafe {std::mem::zeroed()};
-		name.sin_family = AF_INET as u16;
-		{
-			let pv : PVOID = ((&mut name.sin_addr) as * mut IN_ADDR) as PVOID;
-			unsafe {
-				inet_pton(AF_INET,ipaddr.as_bytes().as_ptr() as * const i8,pv);
-			}
-		}
-		name.sin_port = unsafe {htons(port as u16)};
-
 		unsafe {
-			let _funcptr :LPVOID = (&mut retv.acceptfunc as *mut LPFN_CONNECTEX ) as LPVOID;
-			let _funcsize:DWORD = std::mem::size_of::<LPFN_CONNECTEX>() as DWORD;
-			let _guidptr :LPVOID = (&accguid as *const GUID) as LPVOID;
-			let _guidsize :DWORD = std::mem::size_of::<GUID>() as DWORD;
-			let _retptr :LPDWORD = &mut dret;
-			let _ptrov :LPWSAOVERLAPPED = std::ptr::null::<WSAOVERLAPPED>() as LPWSAOVERLAPPED;
-			let _fncall :LPWSAOVERLAPPED_COMPLETION_ROUTINE = None;
-			iret = WSAIoctl(retv.sock,SIO_GET_EXTENSION_FUNCTION_POINTER,_guidptr,_guidsize,_funcptr,_funcsize,_retptr,_ptrov,_fncall);
+			iret = listen(retv.sock,backlog);
+		}
+		if iret == SOCKET_ERROR {
+			ret = get_wsa_errno!();
+			evtcall_new_error!{SockHandleError,"cannot listen error {}",ret}
+
 		}
 
-		if iret != 0 {
-			iret = get_errno!();
-			retv.free();
-			evtcall_new_error!{SockHandleError,"cannot get WSAIoctl error {}",iret}
-		}
-
-		if retv.acceptfunc.is_none() {
-			retv.free();
-			evtcall_new_error!{SockHandleError,"cannot get acceptfunc"}
-		}
-		{
-			let _nameptr :*const SOCKADDR = (&name as *const SOCKADDR_IN) as * const SOCKADDR;
-			let _namelen :c_int = std::mem::size_of::<SOCKADDR_IN>() as c_int;
-			let _sendbuf :LPVOID = std::ptr::null_mut::<c_void>();
-			let _sndsize :DWORD = 0;
-			let _retsend :LPDWORD = &mut dret;
-			let _connovptr :LPOVERLAPPED = &mut retv.connov;
-			unsafe {
-				bret = retv.acceptfunc.as_ref().unwrap()(retv.sock,_nameptr,_namelen,_sendbuf,_sndsize,_retsend,_connovptr);	
-			}
-			if bret == FALSE {
-				iret = get_errno!();
-				retv.free();
-				evtcall_new_error!{SockHandleError,"call acceptfunc error {}",iret}
-			}
-		}
-
-		
+		retv._get_accept_func(&accguid)?;	
 
 
 		Ok(retv)
