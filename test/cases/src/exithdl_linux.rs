0315a4c8532e58f4ec44704c411835aa7e890336
jeppeter <jeppeter@gmail.com>
2024-01-29 12:10:16 +0800

    to make exithdl_linux.rs with EventFd ok

--- test/cases/src/exithdl_linux.rs
+++ test/cases/src/exithdl_linux.rs
@@ -1,10 +1,6 @@
 
 use std::error::Error;
-use std::sync::atomic::{AtomicU16,AtomicU64,Ordering};
-
 use extargsparse_worker::{extargs_error_class,extargs_new_error};
-
-use evtcall::consts::*;
 use evtcall::eventfd::*;
 
 
@@ -13,12 +9,12 @@ extargs_error_class!{SigHdlError}
 //static HANDLE_EXIT :AtomicU64 = AtomicU64::new(INVALID_EVENT_HANDLE);
 //static ST_SIGINITED :AtomicU16 = AtomicU16::new(0);
 
-static EXIT_EVENTFD :Option<EventFd> = None;
+static mut EXIT_EVENTFD :Option<EventFd> = None;
 
 
 fn rust_signal(_iv :libc::c_int) {
-	if EXIT_EVENTFD.is_some() {
-		let r :EventFd = EXIT_EVENTFD.as_ref().unwrap().clone();
+	if unsafe{ EXIT_EVENTFD.is_some()} {
+		let r :EventFd = unsafe {EXIT_EVENTFD.as_ref().unwrap().clone()};
 		let _ = r.set_event();
 	}
 	return;
@@ -35,19 +31,28 @@ fn get_notice_signal() -> libc::sighandler_t {
 
 
 pub fn init_exit_handle() -> Result<u64,Box<dyn Error>> {
-	let mut retv :u64 = INVALID_EVENT_HANDLE;
-	if EXIT_EVENTFD.is_none() {
+	let retv :u64;
+	if unsafe {EXIT_EVENTFD.is_none()} {
 		let cb :EventFd = EventFd::new(0,"exit event")?;	
-		EXIT_EVENTFD = Some(cb.clone());
+
+		let sigret :libc::sighandler_t;
+		unsafe {
+			sigret = libc::signal(libc::SIGINT,get_notice_signal());
+		}
+		if sigret == libc::SIG_ERR {
+			extargs_new_error!{SigHdlError,"cannot set {} signal",libc::SIGINT}
+		}
+
+		unsafe {EXIT_EVENTFD = Some(cb.clone())};
 	}
-	let b = EXIT_EVENTFD.as_ref().unwrap().clone();
+	let b = unsafe{ EXIT_EVENTFD.as_ref().unwrap().clone()};
 	retv = b.get_event();
 	Ok(retv)
 }
 
 pub fn fini_exit_handle() {
-	if EXIT_EVENTFD.is_some(){
-		EXIT_EVENTFD = None;
+	if unsafe{EXIT_EVENTFD.is_some()}{
+		unsafe {EXIT_EVENTFD = None};
 	}
 	return;
 }
